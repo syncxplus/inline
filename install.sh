95fc9dc76870ac716ee45467a05b7f094596c6b3
jibo <jibo@outlook.com>
2018-09-18 09:47:02 +0800

    Release 1.0

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+set -x
+
+export VERSION=1.0
+export SB_VERSION=1.0
+export LOGGING_PATH=/root/logs
+
+function checkCommand {
+    command -v ${1} >/dev/null 2>&1 || {
+        return 1
+    }
+    return 0
+}
+
+function killProcess {
+    pid=$(ps -ef |grep "${1}" |grep -v grep |awk "{print \$2}")
+    if [ ! -z ${pid} ]; then
+        all=(${pid//\s+/ })
+        for id in ${all[@]}
+        do
+            if [ "${id}" -gt 0 ] 2>/dev/null ;then
+                echo "kill ${id}"
+                if [ `uname -s` != Darwin ]; then
+                    pstree -p $id| awk -F"[()]" "{system(\"kill \"\$2)}"
+                else
+                    pstree -g3 -l2 -U $id| awk "{system(\"kill \"\$2)}"
+                fi
+            else
+                echo "ignore ${id}"
+            fi
+        done
+    fi
+}
+
+if checkCommand pstree; then
+    echo pstree already installed
+else
+    if [ `uname -s` != Darwin ]; then
+        yum install -y psmisc
+    else
+        brew install pstree
+    fi
+fi
+
+if checkCommand unzip; then
+    echo unzip already installed
+else
+    yum install -y unzip
+fi
+
+killProcess "inline-${VERSION}.jar"
+[ -e "inline-${VERSION}.jar" ] && rm -rf inline-${VERSION}.jar
+curl -OL https://github.com/syncxplus/inline/releases/download/${VERSION}/inline.zip
+unzip -o inline.zip && rm -rf inline.zip
+curl https://raw.githubusercontent.com/syncxplus/shadowbox/shadowbox/src/server_manager/install_scripts/install_server.sh | bash
+nohup java -jar inline-${VERSION}.jar ${1} &
