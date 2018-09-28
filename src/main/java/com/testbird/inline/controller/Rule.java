998eae0326eb4f8d2dd6c81028f260d67a67f3a6
jibo <jibo@outlook.com>
2018-09-28 17:18:56 +0800

    Format rule according to user-agent

--- src/main/java/com/testbird/inline/controller/Rule.java
+++ src/main/java/com/testbird/inline/controller/Rule.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/rule")
 public class Rule {
@@ -16,7 +18,7 @@ public class Rule {
     }
 
     @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
-    private String display() {
+    private String display(HttpServletRequest request) {
         StringBuilder sb = new StringBuilder("********** tc **********").append(System.lineSeparator());
         String tc = trafficRule.displayTc();
         if (trafficRule.getExitCode() == 0) {
@@ -31,6 +33,8 @@ public class Rule {
         } else {
             sb.append("error:").append(trafficRule.getExitCode());
         }
-        return sb.append(System.lineSeparator()).toString();
+        String result = sb.append(System.lineSeparator()).toString();
+        String userAgent = request.getHeader("User-Agent");
+        return (userAgent != null && userAgent.startsWith("curl")) ? result : result.replaceAll("\n", "<br/>");
     }
 }
