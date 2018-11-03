30b176319983764a2b22dddca60e0c25902819c3
jibo <jibo@outlook.com>
2018-11-03 12:53:22 +0800

    Update ifstat command

--- src/main/java/com/testbird/inline/service/NetworkStat.java
+++ src/main/java/com/testbird/inline/service/NetworkStat.java
@@ -1,6 +1,5 @@
 package com.testbird.inline.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.testbird.inline.metrics.NetworkGauge;
 import com.testbird.inline.metrics.VersionGauge;
 import org.apache.commons.exec.CommandLine;
@@ -14,13 +13,12 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
-import java.util.Map;
 import java.util.Objects;
 
 @Service
 public class NetworkStat {
     private static final Logger logger = LoggerFactory.getLogger(NetworkStat.class);
-    private static final String COMMAND = "ifstat -j eth0";
+    private static final String COMMAND = "ifstat eth0";
     private final NetworkGauge networkGauge;
 
     public NetworkStat(@Autowired NetworkGauge networkGauge) {
@@ -42,7 +40,18 @@ public class NetworkStat {
                 if (exitCode == 0) {
                     String s = output.toString().trim();
                     logger.info(s);
-                    ObjectMapper mapper = new ObjectMapper();
+                    String[] lines = s.split("\n");
+                    if (lines.length > 3) {
+                        String[] eth0 = lines[3].split("\\s+");
+                        if (eth0.length > 7) {
+                            int rxStat = parseStatBytes(eth0[5].toLowerCase());
+                            int txStat = parseStatBytes(eth0[7].toLowerCase());
+                            networkGauge.get().labels("rx").set(rxStat);
+                            networkGauge.get().labels("tx").set(txStat);
+                            networkGauge.get().labels("all").set(rxStat + txStat);
+                        }
+                    }
+                    /*ObjectMapper mapper = new ObjectMapper();
                     Map<String, Map<String, Map>> map = mapper.readValue(s, Map.class);
                     if (map.size() == 1) {
                         Map<String, Map> stat = map.get(map.keySet().iterator().next());
@@ -52,23 +61,38 @@ public class NetworkStat {
                             logger.debug(String.valueOf(eth0.get("tx_bytes")));
                             //double prx = networkGauge.get().labels("rx").get();
                             double rx = Double.parseDouble(String.valueOf(eth0.get("rx_bytes")));
-                            /*if (prx > 0 || rx > prx) {
+                            *//*if (prx > 0 || rx > prx) {
                                 rx -= prx;
-                            }*/
+                            }*//*
                             //double ptx = networkGauge.get().labels("tx").get();
                             double tx = Double.parseDouble(String.valueOf(eth0.get("tx_bytes")));
-                            /*if (ptx > 0 || tx > ptx) {
+                            *//*if (ptx > 0 || tx > ptx) {
                                 tx -= ptx;
-                            }*/
+                            }*//*
                             networkGauge.get().labels("rx").set(rx);
                             networkGauge.get().labels("tx").set(tx);
                             networkGauge.get().labels("all").set(rx + tx);
                         }
-                    }
+                    }*/
                 }
             } catch (Throwable t) {
                 t.printStackTrace();
             }
+        }
+    }
+
+    private static int parseStatBytes(String stat) {
+        try {
+            if (stat.endsWith("m")) {
+                return Integer.parseInt(stat.substring(0, stat.length() - 1)) * 1024 *1024;
+            } else if (stat.endsWith("k")) {
+                return Integer.parseInt(stat.substring(0, stat.length() - 1)) * 1024;
+            } else {
+                return Integer.parseInt(stat);
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+            return  0;
         }
     }
 }
