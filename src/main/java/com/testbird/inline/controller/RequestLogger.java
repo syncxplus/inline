46aa8fd09e7959ec83abb7ce82d9373f56a4fb75
jibo <jibo@outlook.com>
2018-12-12 16:49:51 +0800

    Remove useless metrics

--- src/main/java/com/testbird/inline/controller/RequestLogger.java
+++ src/main/java/com/testbird/inline/controller/RequestLogger.java
@@ -1,21 +1,16 @@
 package com.testbird.inline.controller;
 
-import com.testbird.inline.metrics.HttpRequestCounter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Objects;
 
 import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
 
 @Component
 @Order(HIGHEST_PRECEDENCE)
 public class RequestLogger extends CommonsRequestLoggingFilter {
-    @Autowired
-    private HttpRequestCounter httpRequestCounter;
 
     public RequestLogger() {
         setIncludeClientInfo(true);
@@ -25,9 +20,6 @@ public class RequestLogger extends CommonsRequestLoggingFilter {
     @Override
     protected void beforeRequest(HttpServletRequest request, String message) {
         super.beforeRequest(request, message);
-        if (!Objects.equals(request.getRequestURI(), "/metrics")) {
-            httpRequestCounter.get().labels(request.getMethod(), request.getRequestURI()).inc();
-        }
     }
 
     @Override
