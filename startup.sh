a33a17a571cd0b3c24502e92d9e9f21c50350da3
jibo <jibo@outlook.com>
2018-12-12 10:19:07 +0800

    Version 1.9.3: update tc rules

--- startup.sh
+++ startup.sh
@@ -2,14 +2,9 @@
 set -x
 
 tc qdisc add dev eth0 root handle 1: htb default 1
-tc class add dev eth0 parent 1: classid 1:1 htb rate 1000mbps
-tc class add dev eth0 parent 1:1 classid 1:10 htb rate 1000Kbit ceil 1024Kbit prio 1
-tc class add dev eth0 parent 1:1 classid 1:20 htb rate 2000Kbit ceil 2048Kbit prio 1
-tc class add dev eth0 parent 1:1 classid 1:30 htb rate 3000Kbit ceil 3072Kbit prio 1
-tc class add dev eth0 parent 1:1 classid 1:50 htb rate 5000Kbit ceil 5120Kbit prio 1
-tc class add dev eth0 parent 1:1 classid 1:80 htb rate 8000Kbit ceil 8192Kbit prio 1
-tc class add dev eth0 parent 1:1 classid 1:100 htb rate 10000Kbit ceil 10240Kbit prio 1
+tc class add dev eth0 parent 1: classid 1:1 htb rate 10000mbps
 tc class add dev eth0 parent 1:1 classid 1:200 htb rate 20000Kbit ceil 20480Kbit prio 1
+tc class add dev eth0 parent 1:1 classid 1:800 htb rate 80000Kbit ceil 81920Kbit prio 1
 
 jar=`ls /root/*.jar`
 export LOGGING_PATH=/root/logs
